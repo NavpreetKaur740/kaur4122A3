d6765e0a37fa841d71a39cbf9479184716748fcd
NavpreetKaur740 <52633685+NavpreetKaur740@users.noreply.github.com>
2019-07-23 16:28:54 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import {Kaur4122} from '../../app/kaur4122';
+import { BookInfo } from '../../app/bookInfo';
+
+export const BOOKS: BookInfo[] = 
+
+[
+{   book_name: "The invisible man",
+    book_author: "Ralph Ellison",
+    book_genre: "fiction",
+    book_year: 1897,
+    book_link: "assets/The_Invisible_Man.jpg" },
+
+ {  book_name: "The Story of My Life",
+    book_author: "Helen Keller",
+    book_genre: "Classic",
+    book_year: 1903,
+    book_link: "assets/The_Story_Of_My_Life.jpg" },
+
+{   book_name: "Three Men in a boat",
+    book_author: "Jerome K. Jerome",
+    book_genre: "Comedy",
+    book_year: 1889,
+    book_link: "assets/Three_Men_In_The_Boat.jpg"  },
+
+{   book_name: "To kill a Mocking Bird",
+    book_author: "Harper Lee",
+    book_genre: "Gothic Fiction",
+    book_year: 1960,
+    book_link: "assets/To_Kill.jpg"  }
+
+
+]
